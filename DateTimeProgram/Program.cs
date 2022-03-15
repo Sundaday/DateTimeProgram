c74f3f225c833657a8b59cbb3036c6c6ffeb4e13
Sundaday <john.koch.pro@gmail.com>
2022-03-15 20:43:57 +0100

    date test

---
+++ DateTimeProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace DateTimeProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = DateTime.Now;
+            DateTime tomorrow = date.AddDays(1).AddHours(2).AddMinutes(10);
+
+            CultureInfo langueFr = CultureInfo.GetCultureInfo("de-AT");
+
+            Console.WriteLine(tomorrow);
+            //Console.WriteLine(date.ToString("dddd dd MMMM yyyy HH:mm:ss:ffff", langueFr));
+
+            var diff = tomorrow - date;
+            Console.WriteLine(diff.TotalSeconds);
+        }
+    }
+}
